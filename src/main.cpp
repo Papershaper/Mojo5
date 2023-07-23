e3e5c269d43e95e877f8a6d57cdb8cdfb09091cd
Papershaper <papershaper@gmail.com>
2023-07-23 21:08:26 +0200

    updated for compile, first leg position change.

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,8 @@ Adafruit_PWMServoDriver pca9685 = Adafruit_PWMServoDriver(0x40);
  
 // Range from 0 to 4095  - adjust to define the length of the pulse width
  
-#define SERVOMIN  80  // Minimum value
-#define SERVOMAX  600  // Maximum value
+#define SERVOMIN  100  // Minimum value
+#define SERVOMAX  550  // Maximum value
  
 // Define servo motor connections (expand as required)
 #define SER0  0   //Servo Motor 0 on connector 0
@@ -21,6 +21,7 @@ void setup() {
  
   // Serial monitor setup
   Serial.begin(115200);
+  Serial.setDebugOutput(true);
   Serial.println("PCA9685 Servo Test");
  
   // Init PCA9685
@@ -31,33 +32,43 @@ void setup() {
 
 void loop() {
   // sweep
-    // Move Motor 0 from 0 to 180 degrees
-  for (int posDegrees = 0; posDegrees <= 180; posDegrees++) {
- 
-    // Determine PWM pulse width
-    pwm0 = map(posDegrees, 0, 180, SERVOMIN, SERVOMAX);
-    pwm1 = map(posDegrees, 0, 180, SERVOMIN, SERVOMAX);
-    // Write to PCA9685
+  Serial.println("<===== START MIN =====>");
+  delay(1000);
+    
+  // Initialize positions for Servo 1 and Servo 2
+  int s1posDegrees = 70;
+  int s2posDegrees = 110;
+  
+  // Sweep from s1's minimum to s2's maximum
+  for (; s1posDegrees <= 110 && s2posDegrees >= 70; s1posDegrees++, s2posDegrees--) {
+    // Determine PWM pulse width for Servo 1 and Servo 2
+    int pwm0 = map(s1posDegrees, 0, 180, SERVOMIN, SERVOMAX);
+    int pwm1 = map(s2posDegrees, 0, 180, SERVOMIN, SERVOMAX);
+
+    // Write to PCA9685 for Servo 1 and Servo 2
     pca9685.setPWM(SER0, 0, pwm0);
     pca9685.setPWM(SER4, 0, pwm1);
+
     // Print to serial monitor
-    Serial.print("pos = ");
-    Serial.println(posDegrees);
+    Serial.printf("Servo 1 pos = %d, Servo 2 pos = %d\n", s1posDegrees, s2posDegrees);
     delay(30);
   }
- 
-  // Move Motor 1 from 180 to 0 degrees
-  for (int posDegrees = 180; posDegrees >= 0; posDegrees--) {
- 
-    // Determine PWM pulse width
-    pwm0 = map(posDegrees, 0, 180, SERVOMIN, SERVOMAX);
-    pwm1 = map(posDegrees, 0, 180, SERVOMIN, SERVOMAX);
-    // Write to PCA9685
+
+  Serial.println("<===== Stop Max =====>");
+  delay(1000);
+
+  // Sweep from s1's maximum to s2's minimum
+  for (; s1posDegrees >= 70 && s2posDegrees <= 110; s1posDegrees--, s2posDegrees++) {
+    // Determine PWM pulse width for Servo 1 and Servo 2
+    int pwm0 = map(s1posDegrees, 0, 180, SERVOMIN, SERVOMAX);
+    int pwm1 = map(s2posDegrees, 0, 180, SERVOMIN, SERVOMAX);
+
+    // Write to PCA9685 for Servo 1 and Servo 2
     pca9685.setPWM(SER0, 0, pwm0);
     pca9685.setPWM(SER4, 0, pwm1);
+
     // Print to serial monitor
-    Serial.print("pos = ");
-    Serial.println(posDegrees);
+    Serial.printf("Servo 1 pos = %d, Servo 2 pos = %d\n", s1posDegrees, s2posDegrees);
     delay(30);
   }
 }
